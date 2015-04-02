9d1f2b599fad1a927495c1d0d6ceba11a111f0ee
Adam <accrider@ualr.edu>
2015-04-02 15:05:22 -0400

    Added history to the chat, it is a variable length and can be changed in the index.js file. Need to learn more about Jade so template looks better.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var app = express();
 var port = 3700;
+
+var INIT_MESSAGES = 5;
+
+var messages = new Array()
+
+Array.prototype.inject = function(element) {
+
+    if (this.length >= INIT_MESSAGES) {
+        this.shift()
+    }
+    this.push(element)
+}
  
 //app.get("/", function(req, res){
 //    res.send("It works!");
@@ -21,8 +33,14 @@ var io = require('socket.io').listen(app.listen(port));
 
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', JSON.stringify({ message: 'welcome to the chat' }));
+    socket.emit("init", JSON.stringify(messages))
     socket.on('send', function (data) {
+
+        var message = JSON.parse(data);
+
+        messages.inject(message);
+
         io.sockets.emit('message', data);
     });
 });
--- public/chat.js
+++ public/chat.js
@@ -6,10 +6,20 @@ window.onload = function() {
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
- 
+    socket.on('init', function(data) {
+        var msg = JSON.parse(data);
+	console.log(msg);
+	for (i in msg) {
+		messages.push(msg[i]);
+	}
+	appendMsg(content,messages);
+    
+    }); 
     socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data);
+	var msg = JSON.parse(data);
+	console.log(msg);
+        if(msg.message) {
+            messages.push(msg);
             var html = '';
             for(var i=0; i<messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
@@ -24,10 +34,27 @@ window.onload = function() {
     sendButton.onclick = function() {
 	var test = getParameter("name");
 	if (test) {
-		socket.emit('send', {message:field.value, username:test});
+		socket.emit('send', JSON.stringify({message:field.value, username:test}));
 	}
+	field.value = "";
     };
+    field.onkeypress = function(e) {
+	var test = getParameter("name");
+	if (e.keyCode === 13 && test) {
+	    socket.emit('send', JSON.stringify({message:field.value, username:test}));
+            field.value = "";
+	}
+    }
  
+}
+function appendMsg(content,messages) {
+	var html = '';
+	for (i in messages) {
+		console.log(messages[i]);
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
+	}
+	content.innerHTML = html;
 }
 function getParameter(theParameter) { 
   var params = window.location.search.substr(1).split('&');
